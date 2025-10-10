3470882bc702942b44b0833c677affa1b8bf0d58
Vladimir Deianov <deyanovva@gmail.com>
2025-10-10 02:40:11 -0400

    LBM formula added and a method that shall be called in the map/main added. It is called setup_fluid_grid

--- src/fluid_simulation.rs
+++ src/fluid_simulation.rs
@@ -102,4 +102,79 @@ impl FluidGrid {
         
     }
 
+ /// Add a breach that sucks air out (creates vacuum)
+    pub fn add_breach(&mut self, x: usize, y: usize) 
+    {
+        if x < self.width && y < self.height {
+            self.breaches.push((x, y));
+            info!("Breach created at ({}, {}) ", x, y);
+        }
+    }
+
+    // convert from vector to 2d
+    #[inline]
+    fn get_index(&self, x: usize, y: usize) -> usize 
+    {
+        y * self.width + x
+    }
+
+    // mystirious formula that was passed down from wise men(or women)
+    #[inline]
+    fn compute_equilibrium(&self, density: f32, vx: f32, vy: f32, i: usize) -> f32 
+    {
+        // according to website this is like a dot product of lattice velocity 
+        let cu = C_X[i] * vx + C_Y[i] * vy;
+        // kinetic enegry of the flow
+        let u_sq = vx * vx + vy * vy;
+        //Maxwell-Boltzmann equilibrium formula 
+        // 1 is there even if the velocity is 0, 3 is a coeff for the lattice speed of sound 4.5 * cu * cu is particles gathering together with speed - kinetic enegry
+        WEIGHTS[i] * density * (1.0 + 3.0 * cu + 4.5 * cu * cu - 1.5 * u_sq)
+    }
+
+    fn compute_macroscopic(&self, x: usize, y: usize) -> (f32, f32, f32) {
+        let idx = self.get_index(x, y);
+        
+
+        //these are the accumulators for the velocity. they sum up all the 9 directions
+        let mut rho = 0.0;
+        let mut ux = 0.0;
+        let mut uy = 0.0;
+        
+        for i in 0..9 
+        {
+            //total density is the sum of all distribution functions. Each f[i] tells us how many particles move in direction i, so summing gives total particles in the cell
+            let f = self.distribution[idx][i];
+            rho += f;
+            //momentums in x and y directions
+            ux += C_X[i] * f;
+            uy += C_Y[i] * f;
+        }
+        //check that if the velocity is very small because of the breach, we would rather set it to be a very small number. no division
+        if rho > 0.001 
+        {
+            ux /= rho;
+            uy /= rho;
+        } else 
+        {
+            ux = 0.0;
+            uy = 0.0;
+        }
+        (rho, ux, uy)
+    }
+}
+
+//this method will be called from the map generation
+fn setup_fluid_grid(mut commands: Commands) {
+    let mut grid = FluidGrid::new(GRID_WIDTH, GRID_HEIGHT);
+    grid.initialize_with_perlin(42);
+    
+  
+   
+    //grid.add_breach(10, 48); 
+    //grid.add_breach(118, 48); 
+    
+    commands.spawn((grid, Name::new("FluidGrid")));
+    info!("Fluid simulation with breach aaaaaaaahhhhhh");
+}
+
 }
